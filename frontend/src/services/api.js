d87517240645074af33145a25eb8ffb56afd0f2c
sudhanshu7037 <tomarsudhanshu7037@gmail.com>
2026-05-16 01:35:58 +0530

    edit api.js file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'team-task-manager-production-1cd3.up.railway.app/api',
+  baseURL: 'https://team-task-manager-production-1cd3.up.railway.app/api',
 });
 
 // Add a request interceptor to attach JWT token
